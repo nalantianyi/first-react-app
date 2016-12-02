43dd60838d01e053548c5ef5339b632e843e44da
纳兰天忆 <103520146@qq.com>
2016-12-02 16:57:50 +0800

    init

---
+++ src/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by nalantianyi on 2016/12/2.
+ */
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('app')
+);
